513d0ae1b71b114176ea889405f17da242817f5c
izgutier <izgutier@unirioja.es>
2026-03-22 17:52:43 +0100

    Remove placeholder comments in ContactoSimServiceImpl
    Removed placeholder comments from getEntities and solicitarSimulation methods.

--- src/main/java/servicios/ContantoSimServicioImpl.java
+++ src/main/java/servicios/ContantoSimServicioImpl.java
@@ -11,13 +11,11 @@ public class ContactoSimServiceImpl implements InterfazContactoSim {
 
     @Override
     public List<String> getEntities() {
-        // Inventa una lista de nombres para las entidades (Punto 5)
         return Arrays.asList("Sensor_Norte", "Sensor_Sur", "Reactor_Principal"); [cite: 270]
     }
 
     @Override
     public int solicitarSimulation(DatosSolicitud ds) {
-        // Almacena datos provisionalmente y devuelve un token aleatorio (Punto 5)
         return (int) (Math.random() * 1000); [cite: 269]
     }
 
